357b475a23e9907c4adebdc6dad2787bfc9e2715
montecamo <merrychristek@gmail.com>
2019-08-24 00:57:49 +0300

    add scroll capture listener

---
+++ src/ScrollCapture.js
@@ -0,0 +1,30 @@
+class ScrollCapture {
+  constructor(elem = window, options) {
+    const { x, y, onChange } = options;
+
+    this.onChange = onChange;
+    this.elem = elem;
+
+    this.x = x;
+    this.y = y;
+
+    this.onMouseWheel = this.onMouseWheel.bind(this);
+  }
+
+  onMouseWheel(e) {
+    this.x -= e.deltaX;
+    this.y -= e.deltaY;
+
+    this.onChange([this.x, this.y]);
+  }
+
+  capture() {
+    this.elem.addEventListener('wheel', this.onMouseWheel);
+  }
+
+  stop() {
+    this.elem.removeEventListener('wheel', this.onMouseWheel);
+  }
+}
+
+export default ScrollCapture;
