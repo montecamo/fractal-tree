3273f8845eb03ed7516f92459d11b0fd451e6e45
montecamo <merrychristek@gmail.com>
2019-08-24 01:11:23 +0300

    add zoom capture

---
+++ src/ZoomCapture.js
@@ -0,0 +1,30 @@
+class ScrollCapture {
+  constructor(elem = window, options) {
+    const { zoom, onChange } = options;
+
+    this.onChange = onChange;
+    this.elem = elem;
+
+    this.zoom = zoom;
+
+    this.onMouseWheel = this.onMouseWheel.bind(this);
+  }
+
+  onMouseWheel(e) {
+    if (!e.ctrlKey) return;
+
+    this.zoom -= e.deltaY * 0.1;
+
+    this.onChange(this.zoom);
+  }
+
+  capture() {
+    this.elem.addEventListener('wheel', this.onMouseWheel);
+  }
+
+  stop() {
+    this.elem.removeEventListener('wheel', this.onMouseWheel);
+  }
+}
+
+export default ScrollCapture;
